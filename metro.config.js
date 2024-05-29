a02bbe0065cac75108b073243f80573337f0885a
Michael <segunmichael24@gmail.com>
2024-05-29 12:44:35 +0100

    metro import fix

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-import { getDefaultConfig } from 'expo/metro-config';
+const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-export default config;
+module.exports = config;
