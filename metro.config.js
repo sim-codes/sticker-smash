4970c3f33c32920342b8cedae4a586d386d54e48
Michael <segunmichael24@gmail.com>
2024-05-29 12:42:46 +0100

    metro config

---
+++ metro.config.js
@@ -0,0 +1,7 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+import { getDefaultConfig } from 'expo/metro-config';
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+export default config;
